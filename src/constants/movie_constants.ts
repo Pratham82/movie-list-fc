1197dfc088bf62f99d5c32b4ebcea99860e62ffc
Pratham82 <prathamesh.mali@mosaicwellness.in>
2024-04-26 12:49:31 +0530

    feat: 🎸 added constants
    added constants

---
+++ src/constants/movie_constants.ts
@@ -0,0 +1,9 @@
+const TMDB_MOVIES_BASE_URL = 'https://api.themoviedb.org/3/discover/movie'
+const TMDB_MOVIE_PREFIX = 'https://image.tmdb.org/t/p/w1280'
+const TMDB_GENRE_URL = 'https://api.themoviedb.org/3/genre/movie/list'
+
+export { TMDB_MOVIES_BASE_URL, TMDB_MOVIE_PREFIX, TMDB_GENRE_URL }
+
+// https://api.themoviedb.org/3/discover/movie?api_key=2dca580c2a14b55200e784d157207b4d&sort_by=popularity.desc&primary_release_year=2023&page=1&vote_count.gte=100
+
+//
